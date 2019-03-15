50ae9f9381f9a40ba151edd19a1931591fd70981
wanxuping <977741432@qq.com>
2019-03-15 21:42:04 +0800

    home page

---
+++ spring-cloud-service-restful-example/user-web-register/src/main/java/com/wanxp/userwebregister/UserWebRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.wanxp.userwebregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWebRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWebRegisterApplication.class, args);
+    }
+
+}
--- thymeleaf/src/main/java/com/wanxp/thymeleafdemo/controller/HomeController.java
+++ thymeleaf/src/main/java/com/wanxp/thymeleafdemo/controller/HomeController.java
@@ -7,10 +7,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class HomeController implements IGTVController{
     public void process(HttpServletRequest request, HttpServletResponse responce, ServletContext servletContext, ITemplateEngine templateEngine) throws IOException {
         WebContext context = new WebContext(request, responce, servletContext, request.getLocale());
+        context.setVariable("today", Calendar.getInstance());
         templateEngine.process("home", context, responce.getWriter());
     }
 }
